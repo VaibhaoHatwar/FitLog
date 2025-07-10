fdbd1bb60e8dd3bd1e0590c9d8316a288dbd5228
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-10 19:35:00 +0530

    chore(backend): add dotenv config and request logger middleware
    - Installed dotenv and loaded environment variables using require('dotenv').config()
    - Added custom middleware to log incoming request path and method
    - Updated server to listen on dynamic port from .env

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // exoress app
 const app = express()
 
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+ 
 // routes
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'})
 })
 
 // listen for requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log('listening on port 4000')
 })
