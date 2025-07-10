e2de164344bed76082cb5aef7c8da9e0ff3580c8
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-10 21:58:43 +0530

    feat(backend): connect to MongoDB using mongoose
    - Installed and imported mongoose for MongoDB connection
    - Connected to the database using MONGO_URI from .env
    - Moved app.listen() inside mongoose connection success block
    - Handled connection errors with a catch block

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,30 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const workoutRoutes = require('./routes/workoutRoutes')
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workoutRoutes");
 // exoress app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
- 
-// routes
-app.use('/api/workouts',workoutRoutes)
+  console.log(req.path, req.method);
+  next();
+});
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000')
-})
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
