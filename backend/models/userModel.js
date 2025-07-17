700ee04b9fddc730cc8010f2d9e9d67ddeda9c92
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-17 18:09:13 +0530

    feature: (backend) add user login functionality with static method in userModel
    - Added loginUser controller to handle user login requests
    - Implemented User.login static method in userModel for credential validation
    - Used bcrypt to compare hashed passwords securely
    - Generated JWT token on successful login and returned email + token
    - Included proper error responses for invalid input and failed auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const validator = require("validator")
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -17,20 +17,19 @@ const userSchema = new Schema({
 });
 
 // static signup method
-userSchema.statics.signup = async function(email, password) {
+userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
 
-    // validation
-    if (!email || !password) {
-        throw Error('All fields must be filled')
-    }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
 
-    if (!validator.isEmail(email)) {
-        throw Error('Email is not valid')
-    }
-
-    if (!validator.isStrongPassword(password)) {
-        throw Error('Password not strong enough')
-    }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
 
   const exists = await this.findOne({ email });
 
@@ -42,6 +41,30 @@ userSchema.statics.signup = async function(email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect email");
+    // throw Error("Invalid login credentials!");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+    // throw Error("Invalid login credentials!");
+  }
 
   return user;
 };
