1c7098455983fa838383419a0873eada20a4aff1
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-18 22:13:11 +0530

    feat(frontend): implement signup hook and integrate with SignupPage
    - Created useSignup.jsx to handle signup logic with async/await
    - Fixed typo and added correct useAuthContext.jsx hook
    - Integrated useSignup into SignupPage with loading and error state
    - Conditionally render dynamic error messages in UI

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const SignupPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { signup, isLoading, error } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("email: ", email);
-    console.log("password: ", password);
+    await signup(email, password);
   };
 
   return (
@@ -27,7 +28,8 @@ const SignupPage = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button type="submit">Sign up</button>
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
