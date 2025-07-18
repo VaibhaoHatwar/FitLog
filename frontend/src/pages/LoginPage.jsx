6eaf4e621c1c1f536e2567a6acd04e9a04f3a9fa
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-19 00:05:38 +0530

    feature: (frontend) create useLogin hook and update LoginPage
    - Created a reusable useLogin hook for handling login logic
    - Updated LoginPage.jsx to use the new hook for cleaner code structure
    - Handles auth state update and localStorage set on successful login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login , error,  isLoading } = useLogin()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("email: ", email);
-    console.log("password: ", password);
+    await login(email, password)
   };
 
   return (
@@ -27,7 +28,8 @@ const LoginPage = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button type="submit">Log in</button>
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
