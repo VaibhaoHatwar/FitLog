6952f7092d3b48e5c371ea71892a0c0c199230c0
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-19 17:18:53 +0530

    feature: (frontend) protect routes based on auth and minor navbar style tweak
    - Protected routes in App.jsx using Navigate to redirect based on auth state
    - Redirected unauthenticated users away from HomePage
    - Prevented access to login/signup if user is already logged in
    - Added left margin to navbar button in index.css for better spacing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // pages and components
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import SignupPage from "./pages/SignupPage"
-import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
-      <div className="pages">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-      </div>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <HomePage /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <LoginPage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <SignupPage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
