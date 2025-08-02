54d2be0dcd3db0b746e7c9a8c6513db6e2c1ba77
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-08-02 18:19:41 +0530

    feat(backend): add get and create workout controller functions
    - Implemented getWorkouts to fetch all workouts from the database
    - Implemented getWorkout to fetch a single workout by ID
    - Implemented createWorkout to add a new workout document
    - Verified all endpoints using Postman for expected behavior

--- backend/src/controllers/workoutController.js
+++ backend/src/controllers/workoutController.js
@@ -1,21 +1,55 @@
-import { Workout } from "../models/workoutModel.js"
+import mongoose from "mongoose";
+import { Workout } from "../models/workoutModel.js";
 
-export const getWorkouts = (req, res) => {
-  res.json({ mssg: "Get all workouts" });
+export const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(workouts);
 };
 
-export const getWorkout = (req, res) => {
-  res.json({ mssg: "Get a  single workout" });
+export const getWorkout = async (req, res) => {
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findById(id);
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
 };
 
 export const createWorkout = async (req, res) => {
-  const { title, load, reps } = req.body;
+  const {
+    title,
+    reps,
+    load,
+    category,
+    intensity,
+    notes,
+    workoutDate,
+    caloriesBurned,
+  } = req.body;
 
+  // add doc to db
   try {
-    const workout = await Workout.create({title, load, reps})
+    const workout = await Workout.create({
+      title,
+      load,
+      reps,
+      category,
+      intensity,
+      notes,
+      workoutDate,
+      caloriesBurned,
+    });
     res.status(200).json(workout);
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
 };
 
